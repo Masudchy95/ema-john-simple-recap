ed4d2daa653e2af4bb97f1a4d5eee742274126da
Masud Chy <masud3e.095@gmail.com>
2021-06-01 16:53:21 +0600

    2nd commit after editing

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../../images/logo.png';
+import css from './Header.css';
+const Header = () => {
+    return (
+        <div className="header">
+           <img src={logo} alt="" />
+           <nav>
+               <a href="/shop">Shop</a>          
+               <a href="/review">Order Review</a>
+               <a href="/inventory">Manage Inventory</a>
+            </nav>
+            <input type="text" placeholder="Type here to search"/>
+        </div>
+    );
+};
+
+export default Header;
